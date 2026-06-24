1274df0a9f751c8c53baffb7413bde8882c87857
Nicholas Wade Miller <36825393+nwm516@users.noreply.github.com>
2026-06-24 16:54:58 -0700

    Add: delete-node-in-a-bst - submission-2

---
+++ Data Structures & Algorithms/delete-node-in-a-bst/submission-2.java
@@ -0,0 +1,101 @@
+/*
+Delete Node in a BST
+
+You are given a root node reference of a BST and a key, delete the node with the given key in the BST, if present. Return the root node reference (possibly updated) of the BST.
+
+Basically, the deletion can be divided into two stages:
+
+    Search for a node to remove.
+    If the node is found, delete the node.
+
+Note: There can be multiple results after deleting the node, return any one of them.
+
+Example 1:
+
+Input: root = [5,3,9,1,4], key = 3
+
+Output: [5,4,9,1]
+
+Explanation: Another valid answer is:
+
+Example 2:
+
+Input: root = [5,3,6,null,4,null,10,null,null,7], key = 3
+
+Output: [5,4,6,null,null,null,10,7]
+
+Constraints:
+
+    0 <= The number of nodes in the tree <= 10,000.
+    -100,000 <= key, Node.val <= 100,000
+    All the values Node.val are unique.
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+ /*
+Recursion II Solution
+
+Restructuring of the tree directly can happen instead of copying the successor's value and deleting it separately.
+When the node-for-deletion has both children, we find the in-order successor (leftmost node in the right subtree)
+and attach the left subtree of the deleted node as the left child of this successor. Then, we return the right
+subtree as the replacement.
+
+This avoids copying and removes the node in a single pass through the tree.
+
+Approach:
+
+Return null if root is null.
+If key is greater than root's value, recursively delete from the right subtree.
+Less than? Recursively delete from left subtree.
+
+If key matches root's value:
+    - no left child? Return right child.
+    - no right child? Return left child.
+    - Otherwise, find in-order successor, attach deleted node's left subtre to this successor's left,
+    delete the node, and return right subtree.
+
+Return the root.
+
+Time & Space: O(h)
+
+ */
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) return root;
+
+        if (key > root.val) {
+            root.right = deleteNode(root.right, key);
+        } else if (key < root.val) {
+            root.left = deleteNode(root.left, key);
+        } else {
+            if (root.left == null) return root.right;
+            if (root.right == null) return root.left;
+
+            TreeNode cur = root.right;
+            while (cur.left != null) {
+                cur = cur.left;
+            }
+            cur.left = root.left;
+            TreeNode res = root.right;
+            root = null;
+            return res;
+        }
+        return root;
+    }
+}
