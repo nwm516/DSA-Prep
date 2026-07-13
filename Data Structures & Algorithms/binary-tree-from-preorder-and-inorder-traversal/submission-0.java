cdc6193acec1f6e703886d1a7cb0231b3b504dd0
Nicholas Wade Miller <36825393+nwm516@users.noreply.github.com>
2026-07-13 10:46:16 -0700

    Add problem description and comments to submission-0.java
    Added comments and problem description for clarity.

--- Data Structures & Algorithms/binary-tree-from-preorder-and-inorder-traversal/submission-0.java
+++ Data Structures & Algorithms/binary-tree-from-preorder-and-inorder-traversal/submission-0.java
@@ -1,3 +1,37 @@
+/*
+NeetCode.io Logo
+Construct Binary Tree from Preorder and Inorder Traversal
+Medium Topics Company Tags
+Hints
+
+You are given two integer arrays preorder and inorder.
+
+    preorder is the preorder traversal of a binary tree
+    inorder is the inorder traversal of the same tree
+    Both arrays are of the same size and consist of unique values.
+
+Rebuild the binary tree from the preorder and inorder traversals and return its root.
+
+Example 1:
+
+Input: preorder = [1,2,3,4], inorder = [2,1,3,4]
+
+Output: [1,2,3,null,null,null,4]
+
+Example 2:
+
+Input: preorder = [1], inorder = [1]
+
+Output: [1]
+
+Constraints:
+
+    1 <= inorder.length <= 1000.
+    inorder.length == preorder.length
+    -1000 <= preorder[i], inorder[i] <= 1000
+ */
+
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
